7a323673a071bed6650141b3ef822323c2ca214d
Leonardo Szarblewski <leonardo27.laurindo@gmail.com>
2025-01-10 21:38:04 -0300

    switch case

---
+++ main.js
@@ -0,0 +1,17 @@
+//Conceitos básicos JS
+
+//Utilizando switch case
+
+let option = 3
+
+switch(option){
+    case 1:
+        console.log("Você escolheu o número 1");
+        break
+    case 2:
+        console.log("Você escolheu o número 2");
+        break
+    default:
+        console.log("Error");
+        break
+}
