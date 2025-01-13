9564fc8f39123cbf111cec57a1d6625d85a19745
Leonardo Szarblewski <leonardo27.laurindo@gmail.com>
2025-01-13 17:01:53 -0300

    metodos de texte

--- main.js
+++ main.js
@@ -50,3 +50,52 @@ form.addEventListener("onsubmit", (e) => {
 
 
 
+
+
+//Métodos de texto
+
+/* Length tamanho do texto
+const message = "bom dia leonardo!!"
+console.log(message.length); */
+
+/*Replace substitui 
+
+
+const message = "olá leonardo"
+console.log(message.replace("olá", "oi"));  */
+
+/* Slice tira uma parte da string
+
+const message = "ola leonardo"
+console.log(message.slice(0, 7));
+
+começa na posição 0 e termina na posição 7 */
+
+/* Padstart tudo que eu quero mudar
+
+const message = "bom dia leonardo"
+console.log(message.padStart("variavel", "x")); */
+
+/* Trim Removo espaço em branco na string
+
+const message = "olá leonardo"
+console.log(message.trim()); */
+
+/* Split(separa) join(junta) Separando e unindo strings 
+const message = "estudar, aprender, praticar"
+const separate = message.split(",")
+console.log(separate);
+
+const junta = separate.join(" e")
+console.log(junta); */
+
+/* IndexOf da a posição do indice por exemplo JS está a 16 casas do indice 0
+const message = "estou estudando JS"
+console.log(message.indexOf("JS")); */
+
+/* Includes verifica se existe a palavra e retorna true ou false
+const message = "bom dia leonardo"
+console.log(message.includes("dia")); */
+
+
+
